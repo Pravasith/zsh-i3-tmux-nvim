890f6c76546746a5c6588a792be461ced66cedaf
__prav__ <pravasith@gmail.com>
2025-02-04 13:28:23 -0500

    feat: excluded folders in find_files telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,18 +1,29 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.find_files, {})
+local flags = {
+	additional_args = function()
+		return {
+			"--glob=!**/node_modules/**", -- Add = to combine flag + pattern
+			"--glob=!**/vendor/**",
+			"--glob=!**/third-party/**",
+			"--glob=!**/submodules/**",
+		}
+	end,
+}
+
+vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
-vim.keymap.set("n", "<leader>fl", function()
-	builtin.live_grep({
-		additional_args = function()
-			return {
-				"--glob=!**/node_modules/**", -- Add = to combine flag + pattern
-				"--glob=!**/vendor/**",
-				"--glob=!**/third-party/**",
-				"--glob=!**/submodules/**",
-			}
-		end,
+vim.keymap.set("n", "<leader>ff", function()
+	builtin.find_files({
+		file_ignore_patterns = {
+			"submodules/",
+			"node_modules/",
+			"%.git/", -- Escape dot with % for .git/
+			"vendor/", --
+		},
 	})
+end, {})
+vim.keymap.set("n", "<leader>fl", function()
+	builtin.live_grep(flags)
 end, {})
