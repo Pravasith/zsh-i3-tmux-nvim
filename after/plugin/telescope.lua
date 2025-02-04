3d32ed806bc14c051faa274fd062267c15c3ef79
__prav__ <pravasith@gmail.com>
2025-02-04 13:07:45 -0500

    feat: excluded vendor & submodules from ripgrep search in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,4 +4,15 @@ vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
-vim.keymap.set("n", "<leader>fl", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fl", function()
+	builtin.live_grep({
+		additional_args = function()
+			return {
+				"--glob=!**/node_modules/**", -- Add = to combine flag + pattern
+				"--glob=!**/vendor/**",
+				"--glob=!**/third-party/**",
+				"--glob=!**/submodules/**",
+			}
+		end,
+	})
+end, {})
